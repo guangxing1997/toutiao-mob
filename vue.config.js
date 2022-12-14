f7690b8d7b584b38e6f549bb2119c79186083556
niegx <1050080947@qq.com>
2022-12-14 09:42:53 +0800

    初始化项目目录

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  devServer: { // 自定义服务配置
+    host: 'localhost',
+    open: true,
+    port: 3000
+  },
+  lintOnSave: false // 关闭eslint检查
+}
